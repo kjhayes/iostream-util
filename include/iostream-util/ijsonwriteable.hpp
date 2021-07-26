507c8d2265d4c957ed0745e5c38b0e162a4dbe96
Kevin Hayes <kjhayes47@yahoo.com>
2021-07-26 17:17:11 -0400

    JSON Writer Class

--- include/iostream-util/ijsonwriteable.hpp
+++ include/iostream-util/ijsonwriteable.hpp
@@ -14,50 +14,6 @@ struct JSONFormatting {
 class IJSONWriteable {
 public:
     virtual void WriteJSON(std::ostream& ostr, JSONFormatting formatting, const bool& is_last = false) const = 0;
-
-    static void WriteJSONObject(std::ostream& ostr, const char* name, const IJSONWriteable& obj, JSONFormatting formatting) {
-        for(int i = 0; i < formatting.spacing; i++){ostr<<std::endl;}
-        for(int i = 0; i < formatting.depth; i++){ostr<<"\t";}
-        ostr<<"\""<<name<<"\": {";
-        if(!formatting.write_objects_inline){ostr<<std::endl;}
-        if(formatting.increase_object_element_depth){formatting.depth++;}
-        obj.WriteJSON(ostr, formatting);
-        if(formatting.increase_object_element_depth){formatting.depth--;}
-        for(int i = 0; i < formatting.depth; i++){ostr<<"\t";}
-        ostr<<"}";
-        if(!formatting.write_objects_inline){ostr<<std::endl;}
-    }
-    static void WriteJSONString(std::ostream& ostr, const char* name, const char* string, JSONFormatting formatting, const bool& is_last = false) {
-        for(int i = 0; i < formatting.spacing; i++){ostr<<std::endl;}
-        for(int i = 0; i < formatting.depth; i++){ostr<<"\t";}
-        ostr<<"\""<<name<<"\": \""<<string<<"\"";
-        if(!is_last){ostr<<", ";}
-        if(!formatting.write_objects_inline){ostr<<std::endl;}
-    }
-    template<class T>
-    static void WriteJSONUnsigned(std::ostream& ostr, const char* name, const T& unsigned_number, JSONFormatting formatting, const bool& is_last = false) {
-        for(int i = 0; i < formatting.spacing; i++){ostr<<std::endl;}
-        for(int i = 0; i < formatting.depth; i++){ostr<<"\t";}
-        ostr<<"\""<<name<<"\": "<<((uintmax_t)unsigned_number);
-        if(!is_last){ostr<<", ";}
-        if(!formatting.write_objects_inline){ostr<<std::endl;}
-    }
-    template<class T>
-    static void WriteJSONSigned(std::ostream& ostr, const char* name, const T& signed_number, JSONFormatting formatting, const bool& is_last = false) {
-        for(int i = 0; i < formatting.spacing; i++){ostr<<std::endl;}
-        for(int i = 0; i < formatting.depth; i++){ostr<<"\t";}
-        ostr<<"\""<<name<<"\": "<<((intmax_t)signed_number);
-        if(!is_last){ostr<<", ";}
-        if(!formatting.write_objects_inline){ostr<<std::endl;}
-    }
-    template<class T>
-    static void WriteJSONHexString(std::ostream& ostr, const char* name, const T& hex_num, JSONFormatting formatting, const bool& is_last = false) {
-        for(int i = 0; i < formatting.spacing; i++){ostr<<std::endl;}
-        for(int i = 0; i < formatting.depth; i++){ostr<<"\t";}
-        ostr<<"\""<<name<<"\": \"0x"<<std::hex<<((uintmax_t)hex_num)<<std::dec<<"\"";
-        if(!is_last){ostr<<", ";}
-        if(!formatting.write_objects_inline){ostr<<std::endl;}
-    }
 };
 
 }
