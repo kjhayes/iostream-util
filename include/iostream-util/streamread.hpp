c0f9815e2f0be713fc15694d1f159901ecf93de9
Kevin Hayes <kjhayes47@yahoo.com>
2021-07-26 21:11:18 -0400

    Big Endian Error

--- include/iostream-util/streamread.hpp
+++ include/iostream-util/streamread.hpp
@@ -16,19 +16,19 @@ uint8_t GetNextU8(std::istream& istr, std::ostream& err = std::cerr) {
 }
 
 //Big Endian
-uint16_t GetNextBEU16(std::istream& istr){
-    uint16_t u = ((uint16_t)GetNextU8(istr)) << 8;
-    u |= (uint16_t)GetNextU8(istr);
+uint16_t GetNextBEU16(std::istream& istr, std::ostream& err = std::cerr){
+    uint16_t u = ((uint16_t)GetNextU8(istr, err)) << 8;
+    u |= (uint16_t)GetNextU8(istr, err);
     return u;   
 }
-uint32_t GetNextBEU32(std::istream& istr){
-    uint32_t u = ((uint32_t)GetNextBEU16(istr)) << 16;
-    u |= (uint32_t)GetNextBEU16(istr);
+uint32_t GetNextBEU32(std::istream& istr, std::ostream& err = std::cerr){
+    uint32_t u = ((uint32_t)GetNextBEU16(istr, err)) << 16;
+    u |= (uint32_t)GetNextBEU16(istr, err);
     return u;
 }
-uint64_t GetNextBEU64(std::istream& istr){
-    uint64_t u = ((uint64_t)GetNextBEU32(istr)) << 32;
-    u |= (uint64_t)GetNextBEU32(istr);    
+uint64_t GetNextBEU64(std::istream& istr, std::ostream& err = std::cerr){
+    uint64_t u = ((uint64_t)GetNextBEU32(istr, err)) << 32;
+    u |= (uint64_t)GetNextBEU32(istr, err);    
     return u;
 }
 
