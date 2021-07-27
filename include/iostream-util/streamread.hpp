52da310180bf425e2d32267b30503f72d638d596
Kevin Hayes <kjhayes47@yahoo.com>
2021-07-26 22:45:20 -0400

    Inline All

--- include/iostream-util/streamread.hpp
+++ include/iostream-util/streamread.hpp
@@ -16,34 +16,34 @@ inline uint8_t GetNextU8(std::istream& istr, std::ostream& err = std::cerr) {
 }
 
 //Big Endian
-uint16_t GetNextBEU16(std::istream& istr, std::ostream& err = std::cerr){
+inline uint16_t GetNextBEU16(std::istream& istr, std::ostream& err = std::cerr){
     uint16_t u = ((uint16_t)GetNextU8(istr, err)) << 8;
     u |= (uint16_t)GetNextU8(istr, err);
     return u;   
 }
-uint32_t GetNextBEU32(std::istream& istr, std::ostream& err = std::cerr){
+inline uint32_t GetNextBEU32(std::istream& istr, std::ostream& err = std::cerr){
     uint32_t u = ((uint32_t)GetNextBEU16(istr, err)) << 16;
     u |= (uint32_t)GetNextBEU16(istr, err);
     return u;
 }
-uint64_t GetNextBEU64(std::istream& istr, std::ostream& err = std::cerr){
+inline uint64_t GetNextBEU64(std::istream& istr, std::ostream& err = std::cerr){
     uint64_t u = ((uint64_t)GetNextBEU32(istr, err)) << 32;
     u |= (uint64_t)GetNextBEU32(istr, err);    
     return u;
 }
 
 //Little Endian
-uint16_t GetNextLEU16(std::istream& istr, std::ostream& err = std::cerr){
+inline uint16_t GetNextLEU16(std::istream& istr, std::ostream& err = std::cerr){
     uint16_t u = (uint16_t)GetNextU8(istr);
     u |= (((uint16_t)GetNextU8(istr)) << 8);
     return u;
 }
-uint32_t GetNextLEU32(std::istream& istr, std::ostream& err = std::cerr){
+inline uint32_t GetNextLEU32(std::istream& istr, std::ostream& err = std::cerr){
     uint32_t u = (uint32_t)GetNextLEU16(istr);
     u |= (((uint32_t)GetNextLEU16(istr)) << 16);
     return u;
 }
-uint64_t GetNextLEU64(std::istream& istr, std::ostream& err = std::cerr){
+inline uint64_t GetNextLEU64(std::istream& istr, std::ostream& err = std::cerr){
     uint64_t u = (uint64_t)GetNextLEU32(istr);
     u |= (((uint64_t)GetNextLEU32(istr)) << 32);
     return u;
